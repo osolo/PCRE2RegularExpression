bde81a757d1f1a62ffbe5d1ad833c476fd509a1b
Oz Solomon <ozs@sgstudios.com>
2016-04-02 18:00:46 -0400

    Initial Commit

---
+++ PCRE2RegularExpression/PCRE2RegularExpression.h
@@ -0,0 +1,19 @@
+//
+//  PCRE2RegularExpression.h
+//  PCRE2RegularExpression
+//
+//  Created by Oz Solomon on 2016-04-02.
+//  Copyright © 2016 Social Graph Studios. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PCRE2RegularExpression.
+FOUNDATION_EXPORT double PCRE2RegularExpressionVersionNumber;
+
+//! Project version string for PCRE2RegularExpression.
+FOUNDATION_EXPORT const unsigned char PCRE2RegularExpressionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PCRE2RegularExpression/PublicHeader.h>
+
+
